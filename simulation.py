d48fe97d86875f6a79d4e8073560e9e2edac661a
manhhv87 <manhhv87@gmail.com>
2022-05-27 06:45:25 +0700

    modify

--- simulation.py
+++ simulation.py
@@ -245,11 +245,11 @@ def model_run(model_name, transfer, noise):
 
         # print(all_tensor_array)
 
-        print(all_tensor_array[:,0])
+        print(all_tensor_array[:,5])
 
-        train_tensor_array = all_tensor_array[all_tensor_array[:,0] == 'train']
-        valid_tensor_array = all_tensor_array[all_tensor_array[:,0] == 'valid']
-        test_tensor_array = all_tensor_array[all_tensor_array[:,0] == 'test']
+        train_tensor_array = all_tensor_array[all_tensor_array[:,5] == 'train']
+        valid_tensor_array = all_tensor_array[all_tensor_array[:,5] == 'valid']
+        test_tensor_array = all_tensor_array[all_tensor_array[:,5] == 'test']
 
 
         
